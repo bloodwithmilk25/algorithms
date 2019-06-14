55fbce25fb3551ab5687fb39bb62b9c60736d06d
Nikita <humapen@gmail.com>
2019-06-14 23:12:36 +0300

    Update buble_sort.py

--- buble_sort.py
+++ buble_sort.py
@@ -1,6 +1,6 @@
 def bubble_sort(arr):
   for i in range(len(arr)-1):
-    for n in range(len(arr)-1):
+    for n in range(len(arr)-i-1):
       if arr[n] > arr[n+1]:
         arr[n], arr[n+1] = arr[n+1], arr[n]
   return arr
