1d67964dbfa25eeab9ff730ef89be236d17b610d
Nikita <humapen@gmail.com>
2019-06-19 15:45:46 +0300

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -2,7 +2,7 @@ def binary_search(arr, value):
     low = 0
     high = len(arr) - 1
     while low <= high:
-        mid = (low + high) / 2
+        mid = (low + high) // 2
         guess = arr[mid]
         if guess == value:
             return mid
