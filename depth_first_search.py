f0045eac086dd940c333abab0ed84c6528399a97
nikitatonkoshkur <tonkoshkur@oneplanetops.com>
2022-07-09 15:10:19 +0300

    lifoqueue change to deque in DFS

--- depth_first_search.py
+++ depth_first_search.py
@@ -1,4 +1,4 @@
-from queue import LifoQueue
+from collections import deque
 
 from binary_search_tree import plant_tree, BinarySearchTree, Node
 
@@ -9,15 +9,15 @@ def dfs(tree: BinarySearchTree, value):
     """
     preorder dfs
     """
-    nodes_stack = LifoQueue()
-    nodes_stack.put(tree.root)
+    nodes_stack = deque()
+    nodes_stack.append(tree.root)
     while not nodes_stack.empty():
-        current_node = nodes_stack.get()
+        current_node = nodes_stack.pop()
         print(current_node.value)
         if current_node.value == value:
             return value
 
         if current_node.right:
-            nodes_stack.put(current_node.right)
+            nodes_stack.append(current_node.right)
         if current_node.left:
-            nodes_stack.put(current_node.left)
+            nodes_stack.append(current_node.left)
